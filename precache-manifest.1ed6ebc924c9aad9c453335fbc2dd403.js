504851d82b557533c52262a104381d2b93078b6c
Brian Sunbury <bsunbury29@gmail.com>
2019-04-21 13:32:21 -0700

    Updates

---
+++ precache-manifest.1ed6ebc924c9aad9c453335fbc2dd403.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82944dcf6b132e2aaa71",
+    "url": "/gameboard/static/js/runtime~main.baafba1d.js"
+  },
+  {
+    "revision": "4a234832262836999494",
+    "url": "/gameboard/static/js/main.a0d57995.chunk.js"
+  },
+  {
+    "revision": "eae69f7e37aebc87057e",
+    "url": "/gameboard/static/js/2.1f074609.chunk.js"
+  },
+  {
+    "revision": "4a234832262836999494",
+    "url": "/gameboard/static/css/main.c6052e58.chunk.css"
+  },
+  {
+    "revision": "d3802168e74ee63c7953cb3a1878d819",
+    "url": "/gameboard/index.html"
+  }
+];
