42731bdb51cfb78acd681fe29fb52d20bda3b024
Brian Sunbury <bsunbury29@gmail.com>
2019-04-21 14:30:56 -0700

    removed source files

--- src/ApiHandler/EndPoints.js
+++
@@ -1,7 +0,0 @@
-const prod = true;
-const baseUrl = prod ? 'https://gameboard-238222.appspot.com' : 'http://10.0.1.16:8080';
-const getGameIdUrl = '/api/get/gameId';
-const createGameUrl = '/api/create/newGame';
-const createPlayerUrl = '/api/create/player';
-
-export { baseUrl, getGameIdUrl, createGameUrl, createPlayerUrl };
