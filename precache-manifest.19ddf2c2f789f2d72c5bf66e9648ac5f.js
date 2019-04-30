9da64e0c53c953747d754caa049639225181d48e
Brian Sunbury <bsunbury29@gmail.com>
2019-04-30 00:49:33 -0700

    no console.log

---
+++ precache-manifest.19ddf2c2f789f2d72c5bf66e9648ac5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82944dcf6b132e2aaa71",
+    "url": "/gameboard/static/js/runtime~main.baafba1d.js"
+  },
+  {
+    "revision": "f428bcb9a53ed69220fe",
+    "url": "/gameboard/static/js/main.a618045b.chunk.js"
+  },
+  {
+    "revision": "9ae930d22423ed7d8ba2",
+    "url": "/gameboard/static/js/2.0c3a471c.chunk.js"
+  },
+  {
+    "revision": "f428bcb9a53ed69220fe",
+    "url": "/gameboard/static/css/main.0bb429a2.chunk.css"
+  },
+  {
+    "revision": "e5a4618cdd1cc7cc9ca21e8eab1b8a94",
+    "url": "/gameboard/index.html"
+  }
+];
